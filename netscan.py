3a6cfb98973ef38ad0f18262eb3ac039d69d704a
johnbreth <jbreth1@gmail.com>
2022-02-23 16:24:48 -0500

    ARP Scanner script

---
+++ netscan.py
@@ -0,0 +1,15 @@
+#ARP scanner that requires input of the interface to listen on and the network range to scan.
+#Example: python3 netscan.py eth0 10.1.1.0/24
+
+from scapy.all import *
+
+interface = {sys.argv[1]}
+ip_range = {sys.argv[2]}
+broadcastMac = "ff:ff:ff:ff:ff:ff"
+
+packet = Ether(dst=broadcastMac)/ARP(pdst = ip_range)
+
+ans, unans = srp(packet, timeout =2, iface=interface, inter=0.1)
+
+for send,receive in ans:
+        print (receive.sprintf(r"%Ether.src% - %ARP.psrc%"))
